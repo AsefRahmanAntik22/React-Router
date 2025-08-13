e1d7eb3eb53679b429fcc6bfc34dff9e1ee4b606
Asef Rahman <asefrahman2001@gmail.com>
2025-08-13 15:02:08 +0600

    Add link and pages

--- react-router-7-practice/src/App.jsx
+++ react-router-7-practice/src/App.jsx
@@ -2,17 +2,21 @@
 import { Route, Routes,Link } from "react-router"
 import Home from "./Home"
 import About from "./About"
+import Login from "./Login"
 function App() {
  
 
   return (
     <>
-    <Link to="/" >Home</Link>
+    <Link to="/" ><h1>Home</h1></Link>
     <br />
-    <Link to="/about" >About</Link>
+    <Link to="/about" ><h1>About</h1></Link>
+    <br />
+    <Link to="/login" ><h1>Login</h1></Link>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
     <h1>React Router Tutorial 7.8.0</h1>
     </>
