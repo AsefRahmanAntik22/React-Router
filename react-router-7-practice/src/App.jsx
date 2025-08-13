9d4e05b8ed877465eb9e76a513f107dba6f40567
Asef Rahman <asefrahman2001@gmail.com>
2025-08-13 15:18:19 +0600

    add Navbar

--- react-router-7-practice/src/App.jsx
+++ react-router-7-practice/src/App.jsx
@@ -1,18 +1,15 @@
 
-import { Route, Routes,Link } from "react-router"
+import { Route, Routes } from "react-router"
 import Home from "./Home"
 import About from "./About"
 import Login from "./Login"
+import NavBar from "./NavBar"
 function App() {
  
 
   return (
     <>
-    <Link to="/" ><h1>Home</h1></Link>
-    <br />
-    <Link to="/about" ><h1>About</h1></Link>
-    <br />
-    <Link to="/login" ><h1>Login</h1></Link>
+    <NavBar />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
